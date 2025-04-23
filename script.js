f67f565cc81a8807953f4ab7c6096289d91a5f57
Ihedioha Ifeyinwa <ihediohaifeyinwa@gmail.com>
2025-04-23 13:53:14 +0100

    feat- first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function joinNow() {
+  alert("Redirecting to sign-up...");
+}
+
+function explore() {
+  alert("Explore the GymVerse – Coming Soon!");
+}
+
+function takeQuiz() {
+  alert("Launching Fit Quiz – Stay tuned!");
+}
